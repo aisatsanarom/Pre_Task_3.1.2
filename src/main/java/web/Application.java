d375483d9a73ff2c231ab4a71499e2b33d49b9cb
isatsanarom <morozova18080108@gmail.com>
2024-06-13 20:24:44 +0300

    3_1_2

--- src/main/java/web/Application.java
+++ src/main/java/web/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+//это проект с гитхаба
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
